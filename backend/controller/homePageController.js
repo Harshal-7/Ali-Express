4f99fa50df617a5f66ffa4d8c90b5a95c79acecc
zoha shaikh <zoha@pleximus.net>
2024-08-04 14:18:08 +0530

    changes in moretolove

--- backend/controller/homePageController.js
+++ backend/controller/homePageController.js
@@ -1,6 +1,21 @@
 import moreToLove from '../data/moreToLove.js'
+const { resultList } = moreToLove
+
+
+// to display random data on every request
+const shuffleArray = (array) => {
+  let shuffledArray = array.slice(); 
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+};
+
+
 export const getMoreToLove = (req, res) => {
-  res.json(moreToLove);
+  let result = shuffleArray(resultList)
+  res.json(result);
 };
 
 
